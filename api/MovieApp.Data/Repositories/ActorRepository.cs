b336666b10520491c73e6f680a474dcc8e55383a
Shanuka Lakshan <shanuka70710880@gmail.com>
2025-02-10 20:21:41 +0530

    repository created for get user by id, dbcontext created

---
+++ api/MovieApp.Data/Repositories/ActorRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieApp.Data.Entities;
+
+namespace MovieApp.Data.Repositories
+{
+    public class ActorRepository:IActorRepository
+    {
+        private readonly MovieDbContext _movieDbContext;
+
+        public ActorRepository(MovieDbContext movieDbContext)
+        {
+            this._movieDbContext = movieDbContext;
+        }
+
+        public async Task<ActorDataModel> getActorAsync(int id)
+        {
+            var actor = await _movieDbContext.Actors.FirstOrDefaultAsync(u=>u.Id == id);
+            if (actor == null)
+            {
+                throw new Exception("user not found");
+            }
+            else
+            {
+                return actor;
+            }
+                
+                
+
+        }
+    }
+}
---
+++ api/MovieApp.Data/Repositories/IActorRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieApp.Data.Entities;
+
+namespace MovieApp.Data.Repositories
+{
+    public interface IActorRepository
+    {
+
+        //get actor by id
+        Task<ActorDataModel> getActorAsync(int id);
+    }
+}
