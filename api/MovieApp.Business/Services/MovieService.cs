607ea1c37c93f22d0995f24e91af70cc6865ef87
Shanuka Lakshan <shanuka70710880@gmail.com>
2025-02-13 10:14:45 +0530

    get all movies API test cases added

--- api/MovieApp.Business/Services/MovieService.cs
+++ api/MovieApp.Business/Services/MovieService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.Extensions.Logging;
 using MovieApp.Business.DTOs;
 using MovieApp.Data.Repositories;
 
@@ -13,32 +14,33 @@ namespace MovieApp.Business.Services
     {
         private readonly IMovieRepository _movieRepository;
         private readonly IMapper _mapper;
+        private readonly ILogger<MovieService> _logger;
 
-        public MovieService(IMovieRepository movieRepository, IMapper mapper)
+        public MovieService(IMovieRepository movieRepository, IMapper mapper,ILogger<MovieService> @object)
         {
             _movieRepository = movieRepository;
             _mapper = mapper;
+            _logger = @object;
         }
         public async Task<List<MovieInfo>> GetMoviesAsync()
         {
             try
             {
                 var movies = await _movieRepository.GetMoviesAsync();
-                if (movies.Count > 0)
+                if(movies == null||movies.Count==0)
+                {
+                    return new List<MovieInfo>();
+                }else
                 {
                     var movieInfo = _mapper.Map<List<MovieInfo>>(movies);
                     return movieInfo;
                 }
-                else
-                {
-                    return null;
-
-                }
-
+                
             }
             catch (Exception ex)
             {
-                throw new Exception(ex.Message);
+                _logger.LogError($"Error fetching movies: {ex.Message}");
+                return new List<MovieInfo>();
             }
             
         }
