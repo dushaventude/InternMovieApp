d7fe9b4007c8ab57287bf6c9085df533934b4e66
Shanuka Lakshan <121119338+SILVA-HSL@users.noreply.github.com>
2025-02-18 10:06:03 +0530

    get user by id is added( only upto repository)

---
+++ api/MovieApp.Data/Repositories/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieApp.Data.Entities;
+
+namespace MovieApp.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AuthDbContext _context;
+
+        public UserRepository(AuthDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<ApplicationUser> GetUserById(string id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
