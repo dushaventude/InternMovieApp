bad8d703fcd3647beed4241c8f05daa3d06d1637
ashfaqfarleen <ashfaq.f@aventude.com>
2025-02-21 19:41:10 +0530

    carousel done

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/HomePage/HomePage";
 import AppLayout from "./layouts/AppLayout";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
