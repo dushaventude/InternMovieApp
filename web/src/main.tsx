88e75ac4675d92357cebfd6d5f8bb3e7f1d69306
Shanuka Lakshan <121119338+SILVA-HSL@users.noreply.github.com>
2025-02-26 14:10:10 +0530

    redux initiation

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,14 +1,17 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.scss";
-import { Provider } from "react-redux";
-import { store } from "./store/store.tsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { Provider } from 'react-redux';
+import { persistor, store } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import './index.scss';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
---
+++ web/src/models/enum.ts
@@ -0,0 +1,17 @@
+export enum loadingStates {
+    IDLE = 'idle',
+    PENDING = 'pending',
+    LOADING = 'loading',
+    SUCCESS = 'success',
+    FAILURE = 'failure'
+  }
+  
+export enum pagination {
+    PAGE_SIZE = 10
+  }
+  
+  export enum responseStatus {
+    SUCCESS = 'success',
+    ERROR = 'error',
+    WARNING = 'warning'
+  }
